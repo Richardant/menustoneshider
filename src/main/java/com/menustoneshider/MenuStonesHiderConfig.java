a0908c08890d38491080214e6871b7fcb1480e22
Richardant <54707022+Richardant@users.noreply.github.com>
2025-06-29 15:43:49 -0400

    Menu Stones Hider added

---
+++ src/main/java/com/menustoneshider/MenuStonesHiderConfig.java
@@ -0,0 +1,79 @@
+package com.menustoneshider;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Keybind;
+
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+@ConfigGroup("menustoneshider")
+public interface MenuStonesHiderConfig extends Config
+{
+    @ConfigItem(
+            keyName = "hideTopMenu",
+            name = "Hide Top Menu Stones",
+            description = "Configures whether the Top Menu Stones are hidden",
+            position = 0
+    )
+    default boolean hideTopMenu()
+    {
+        return true;
+    }
+
+    @ConfigItem(
+            keyName = "hideBottomMenu",
+            name = "Hide Bottom Menu Stones",
+            description = "Configures whether the Bottom Menu Stones are hidden",
+            position = 1
+    )
+    default boolean hideBottomMenu()
+    {
+        return true;
+    }
+
+    @ConfigItem(
+            keyName = "hideBackground",
+            name = "Hide Background",
+            description = "Configures whether the background of the menu is hidden (Resizable classic only)",
+            position = 2
+    )
+    default boolean hideBackground()
+    {
+        return false;
+    }
+
+    @ConfigItem(
+            keyName = "hideLeftBar",
+            name = "Hide Left Sidebar",
+            description = "Configures whether the left sidebar is hidden (Resizable classic only)",
+            position = 3
+    )
+    default boolean hideLeftBar()
+    {
+        return false;
+    }
+
+    @ConfigItem(
+            keyName = "hideRightBar",
+            name = "Hide Right Sidebar",
+            description = "Configures whether the right sidebar is hidden (Resizable classic only)",
+            position = 4
+    )
+    default boolean hideRightBar()
+    {
+        return false;
+    }
+
+    @ConfigItem(
+            keyName = "showMenuToggle",
+            name = "Show hidden Menu hotkey",
+            description = "Enable hotkey to show all hidden menu widgets",
+            position = 5
+    )
+    default Keybind showMenuToggle()
+    {
+        return new Keybind(KeyEvent.VK_B, InputEvent.CTRL_DOWN_MASK);
+    }
+}
---
+++ src/main/java/com/menustoneshider/MenuStonesHiderPlugin.java
@@ -0,0 +1,119 @@
+package com.menustoneshider;
+
+import com.google.inject.Provides;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Client;
+import net.runelite.api.ScriptID;
+import net.runelite.api.events.ScriptPostFired;
+import net.runelite.api.events.WidgetLoaded;
+import net.runelite.api.gameval.InterfaceID;
+import net.runelite.api.widgets.Widget;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ConfigChanged;
+import net.runelite.client.input.KeyListener;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+import javax.inject.Inject;
+import java.awt.event.KeyEvent;
+
+@Slf4j
+@PluginDescriptor(
+        name = "Menu Stones Hider",
+        description = "Hides menu stones, background and sidebars",
+        tags = {"stone", "hide", "menu", "bars", "background"}
+)
+public class MenuStonesHiderPlugin extends Plugin implements KeyListener
+{
+    @Inject
+    private Client client;
+
+    @Inject
+    private MenuStonesHiderConfig config;
+
+    private boolean toggled = true;
+
+    @Override
+    protected void startUp()
+    {
+        updateOrShowWidgets(true);
+        client.getCanvas().addKeyListener(this);
+    }
+
+    @Override
+    protected void shutDown()
+    {
+        updateOrShowWidgets(false);
+        client.getCanvas().removeKeyListener(this);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e)
+    {
+        if (config.showMenuToggle().matches(e)) {
+            toggled = !toggled;
+            updateOrShowWidgets(toggled);
+        }
+    }
+
+    @Override public void keyTyped(KeyEvent e) { }
+    @Override public void keyReleased(KeyEvent e) { }
+
+    @Subscribe
+    public void onWidgetLoaded(WidgetLoaded event)
+    {
+        if (toggled && (event.getGroupId() == InterfaceID.TOPLEVEL_OSRS_STRETCH || event.getGroupId() == InterfaceID.TOPLEVEL_PRE_EOC))
+        {
+            updateOrShowWidgets(true);
+        }
+    }
+
+    @Subscribe
+    public void onScriptPostFired(ScriptPostFired s)
+    {
+        if (!toggled && s.getScriptId() == 839)
+        {
+            updateOrShowWidgets(false);
+        }
+
+        if (toggled && (s.getScriptId() == ScriptID.TOPLEVEL_REDRAW || s.getScriptId() == 903))
+        {
+            updateOrShowWidgets(true);
+        }
+    }
+
+    @Subscribe
+    public void onConfigChanged(ConfigChanged event)
+    {
+        updateOrShowWidgets(toggled);
+    }
+
+    @Provides
+    MenuStonesHiderConfig provideConfig(ConfigManager configManager)
+    {
+        return configManager.getConfig(MenuStonesHiderConfig.class);
+    }
+
+    private void updateOrShowWidgets(boolean hide)
+    {
+        toggleWidget(InterfaceID.ToplevelOsrsStretch.SIDE_MENU_GRAPHIC1, hide && config.hideLeftBar());
+        toggleWidget(InterfaceID.ToplevelOsrsStretch.SIDE_MENU_GRAPHIC2, hide && config.hideRightBar());
+        toggleWidget(InterfaceID.ToplevelOsrsStretch.SIDE_BACKGROUND, hide && config.hideBackground());
+        toggleWidget(InterfaceID.ToplevelOsrsStretch.SIDE_MENU_GRAPHIC5, hide && config.hideTopMenu());
+        toggleWidget(InterfaceID.ToplevelOsrsStretch.SIDE_TOP, hide && config.hideTopMenu());
+        toggleWidget(InterfaceID.ToplevelPreEoc.SIDE_MOVABLE_LAYER, hide && config.hideTopMenu());
+        toggleWidget(InterfaceID.ToplevelOsrsStretch.SIDE_MENU_GRAPHIC3, hide && config.hideBottomMenu());
+        toggleWidget(InterfaceID.ToplevelOsrsStretch.SIDE_BOTTOM, hide && config.hideBottomMenu());
+        toggleWidget(InterfaceID.ToplevelPreEoc.SIDE_STATIC_LAYER, hide && config.hideBottomMenu());
+    }
+
+    private void toggleWidget(int widgetId, boolean hide)
+    {
+        Widget widget = client.getWidget(widgetId);
+        if (widget != null)
+        {
+            widget.setHidden(hide);
+        }
+    }
+}
